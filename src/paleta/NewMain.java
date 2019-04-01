6cc6c541e7fda0e020c2f5feb2724a2b6456857a
fsancheztemprano <fsancheztemprano@danielcastelao.org>
2019-04-01 11:53:54 +0200

    dupe con paleta

---
+++ src/paleta/NewMain.java
@@ -0,0 +1,12 @@
+package paleta;
+/**
+ * @author fsancheztemprano
+ */
+public class NewMain {
+    public static void main(String[] args) {
+        
+        VentanaP v = new VentanaP();
+        v.setVisible(true);
+
+    }
+}
